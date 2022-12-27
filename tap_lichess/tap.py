568c10dfb241d6e44fbd00f48a6d9d217ff38761
Hichem ELB <hichem@sendinblue.com>
2022-12-27 01:57:07 +0100

    Headers UP (#1)

--- tap_lichess/tap.py
+++ tap_lichess/tap.py
@@ -1,26 +1,20 @@
-"""
-lichess tap class.
-"""
+"""lichess tap class."""
 
 from typing import List
 
-from singer_sdk import Tap, Stream
+from singer_sdk import Stream, Tap
 from singer_sdk import typing as th  # JSON schema typing helpers
 
-from tap_lichess.streams import (
-    lichessStream,
-    GameHistoryStream,
-)
+from tap_lichess.streams import GameHeaderStream
 
 STREAM_TYPES = [
-    GameHistoryStream,
+    GameHeaderStream,
 ]
 
 
 class Taplichess(Tap):
-    """
-    lichess tap class.
-    """
+    """lichess tap class."""
+
     name = "tap-lichess"
 
     config_jsonschema = th.PropertiesList(
@@ -29,37 +23,27 @@ class Taplichess(Tap):
             th.BooleanType,
             required=False,
             default=None,
-            description="The token to authenticate against the API service"
+            description="The token to authenticate against the API service",
         ),
         th.Property(
             "auth_token",
             th.StringType,
             required=False,
             secret=True,  # Flag config as protected.
-            description="The token to authenticate against the API service"
+            description="The token to authenticate against the API service if applicable",
         ),
-        # th.Property(
-        #     "project_ids",
-        #     th.ArrayType(th.StringType),
-        #     required=True,
-        #     description="Project IDs to replicate"
-        # ),
         th.Property(
             "start_date",
             th.DateTimeType,
-            description="The earliest record date to sync"
+            description="The earliest record date to sync",
         ),
         th.Property(
-            "api_url",
-            th.StringType,
-            description="The url for the API service"
+            "api_url", th.StringType, description="The url for the API service"
         ),
     ).to_dict()
 
     def discover_streams(self) -> List[Stream]:
-        """
-        Return a list of discovered streams.
-        """
+        """Return a list of discovered streams."""
         return [stream_class(tap=self) for stream_class in STREAM_TYPES]
 
 
